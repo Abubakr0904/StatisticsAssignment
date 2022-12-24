e0f0b5df3f19616457e97a8040cfbe2941b74570
Abubakr Bakhromov <baxromovabubakr@gmail.com>
2022-12-24 20:25:36 +0500

    Initial Commit

---
+++ StatisticsAssignment/Db/AssignmentDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StatisticsAssignment.Db.Entities;
+
+namespace StatisticsAssignment.Db
+{
+    public sealed class AssignmentDbContext : DbContext
+    {
+        public AssignmentDbContext(DbContextOptions options)
+            :base(options)
+        {
+        }
+
+        public DbSet<CountryData> CountryData { get; set; }
+    }
+}
---
+++ StatisticsAssignment/Db/Entities/CountryData.cs
@@ -0,0 +1,19 @@
+﻿namespace StatisticsAssignment.Db.Entities
+{
+    public class CountryData
+    {
+        public virtual int Id { get; set; }
+
+        public string Country { get; set; }
+
+        public int Year { get; set; }
+
+        public decimal Population { get; set; }
+
+        public decimal AverageAnnualHoursWorkedByPersonsEngaged { get; set; }
+
+        public decimal RealGdp { get; set; }
+
+        public decimal ShareOfHouseholdConsumption { get; set; }
+    }
+}
